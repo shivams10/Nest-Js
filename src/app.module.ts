67f55f27096a720953255e28b374a632c017df3d
Shivam Shukla <shivam@gkmit.co>
2025-02-01 14:31:54 +0530

    CRUD operation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './modules/users/users.controller';
+import { UserResourceController } from './modules/userResource/userResource.controller';
 @Module({
-  controllers: [UsersController],
+  controllers: [UsersController, UserResourceController],
 })
 export class AppModule {}
---
+++ src/dto/create-user-resource.dto.ts
@@ -0,0 +1,5 @@
+export class CreateUserResourceDto {
+  name: string;
+  age: number;
+  id: number;
+}
